e8a89d230c9df8f07821fddafe53596e49f5a531
Marcos L <marcosrlj@outlook.com>
2021-10-19 17:49:11 -0300

    initializing the summation app

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 class App extends React.Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Summation</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ddd',
+    flex: 1,
+  },
+});
 
 export default App;
